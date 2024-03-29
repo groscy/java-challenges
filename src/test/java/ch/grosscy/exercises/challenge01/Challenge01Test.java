540c6fc0082f08b926c986f5a9e5e2365c15711c
Cyril <92256856+groscy@users.noreply.github.com.>
2024-03-29 20:38:18 +0100

    Corrected Challenge01

--- src/test/java/ch/grosscy/exercises/challenge01/Challenge01Test.java
+++ src/test/java/ch/grosscy/exercises/challenge01/Challenge01Test.java
@@ -32,34 +32,19 @@ class Challenge01Test {
     @ParameterizedTest
     @CsvSource({
             "10, 23",
-            "100, 2318",
-            "1000, 233168",
-            "10000, 23331668",
+            "100, 2_318",
+            "1000, 233_168",
+            "10000, 23_331_668",
     })
     void testPositiveCases(int input, int expectedResult) {
         int result = challenge01.calculate(input);
         var smallerThanExpectedResult = new Condition<Integer>(x -> x < expectedResult,
-                                               STR."smaller than:\n\t\{expectedResult}\nImportant: Output is too big do only calculate with numbers below \{input}");
+                                               STR."smaller than:\n\t\{expectedResult}\nImportant: Your result is too small do you have all the numbers divisible by 3 OR 5 in your calculation");
         var greaterThanExpectedResult = new Condition<Integer>(x -> x > expectedResult,
-                                               STR."greater than:\n\t\{expectedResult}\nImportant: Number is too low do you have all the numbers divisible by 3 OR 5 in your calculation");
+                                               STR."greater than:\n\t\{expectedResult}\nImportant: Your result is too big do only calculate with numbers below \{input}");
         assertThat(result)
                 .isNot(smallerThanExpectedResult)
                 .isNot(greaterThanExpectedResult)
                 .isEqualTo(expectedResult);
     }
-
-    @ParameterizedTest
-    @CsvSource({
-            "10, 33",
-            "100, 2418",
-            "1000, 234168",
-            "10000, 23341668",
-    })
-    void testNegativeCase(int input, int output) {
-        assertThat(challenge01.calculate(input))
-                .as("Important: Only numbers below {} the input number should be included!",
-                    input)
-                .isNotEqualTo(output);
-    }
-
 }
