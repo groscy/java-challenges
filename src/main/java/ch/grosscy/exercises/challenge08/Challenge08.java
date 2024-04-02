fa149fa3d2056a2ccc9bf809b055253d11e9db3e
Cyril <92256856+groscy@users.noreply.github.com.>
2024-04-02 22:23:46 +0200

    Added Challenge08

---
+++ src/main/java/ch/grosscy/exercises/challenge08/Challenge08.java
@@ -0,0 +1,13 @@
+package ch.grosscy.exercises.challenge08;
+
+public class Challenge08 implements ImplementationGuide {
+
+    // Use this String to get the 1000-digit number from the challenge description
+    private final String numberToUseForSearchingAdjacentDigits = DIGIT_SEQUENCE;
+
+    @Override
+    public long calculate(int numberOfAdjacentDigits) {
+        // TODO Implementation...
+        return 0L;
+    }
+}
