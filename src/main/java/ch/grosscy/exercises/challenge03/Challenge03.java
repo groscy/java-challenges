7fe97ab1944a5e297864fa2e2e7f7b6d763a1593
Nattaya Buck <cheekygirl765@gmail.com>
2024-04-04 20:56:09 +0200

    Corrected Challenge03

--- src/main/java/ch/grosscy/exercises/challenge03/Challenge03.java
+++ src/main/java/ch/grosscy/exercises/challenge03/Challenge03.java
@@ -1,20 +1,11 @@
 package ch.grosscy.exercises.challenge03;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.LongStream;
 
 public class Challenge03 implements ImplementationGuide {
     @Override
     public List<Long> calculate(long primeNumber) {
         // TODO Implementation...
-        return LongStream.range(2, primeNumber)
-                         .parallel()
-                         .filter(x -> primeNumber % x == 0)
-                         .filter(x -> LongStream.range(2, x - 1)
-                                                .parallel()
-                                                .allMatch(y -> x % y != 0))
-                         .boxed()
-                         .collect(Collectors.toList());
+        return List.of();
     }
 }
