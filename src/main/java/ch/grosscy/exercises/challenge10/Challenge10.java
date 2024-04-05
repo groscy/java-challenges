4f8fc5e29cc4d24ea243746369983a2a1a28070f
Cyril <92256856+groscy@users.noreply.github.com.>
2024-04-05 17:24:26 +0200

    Corrected Challenge10

--- src/main/java/ch/grosscy/exercises/challenge10/Challenge10.java
+++ src/main/java/ch/grosscy/exercises/challenge10/Challenge10.java
@@ -3,6 +3,7 @@ package ch.grosscy.exercises.challenge10;
 public class Challenge10 implements ImplementationGuide {
     @Override
     public long calculate(int upperLimit) {
+        // TODO Implementation...
         return 0L;
     }
 }
