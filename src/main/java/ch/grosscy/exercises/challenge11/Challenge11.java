bfff0150d886aefe4cae7e8ea68287cac2a6cd89
Cyril <92256856+groscy@users.noreply.github.com.>
2024-04-05 17:25:01 +0200

    Added Challenge11

---
+++ src/main/java/ch/grosscy/exercises/challenge11/Challenge11.java
@@ -0,0 +1,11 @@
+package ch.grosscy.exercises.challenge11;
+
+public class Challenge11 implements ImplementationGuide {
+
+    @Override
+    public long calculate(int searchRayLength) {
+        // TODO Implementation...
+        return 0L;
+    }
+
+}
