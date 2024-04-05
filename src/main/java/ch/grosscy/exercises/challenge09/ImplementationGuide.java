aad9bc4083393537fa8a384b07f2800e7e3831f3
Cyril <92256856+groscy@users.noreply.github.com.>
2024-04-05 13:58:58 +0200

    Added Challenge09

---
+++ src/main/java/ch/grosscy/exercises/challenge09/ImplementationGuide.java
@@ -0,0 +1,9 @@
+package ch.grosscy.exercises.challenge09;
+
+import java.util.function.Predicate;
+
+public interface ImplementationGuide {
+
+    long calculate(Predicate<Challenge09.PythagoreanTriplet> pythagoreanTripletPredicate);
+
+}
---
+++ src/test/java/ch/grosscy/exercises/challenge09/Challenge09Test.java
@@ -0,0 +1,49 @@
+package ch.grosscy.exercises.challenge09;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * <b>Problem description</b>
+ * <p>
+ * A Pythagorean triplet is a set of three natural numbers,
+ * <i>a < b < c</i>, for which,
+ * <br/>
+ * <br/>
+ * <i>a<sup>2</sup> + b<sup>2</sup> = c<sup>2</sup></i>
+ * <br/>
+ * <br/>ö
+ * For example, <i>3<sup>2</sup> + 4<sup>2</sup> = 16 = 25 = 5<sup>2</sup></i> .
+ * <br/>
+ * <br/>
+ * There exists exactly one pythagorean triplet for which <i>a + b + c = 1000</i>.
+ * Find the product <i>abc</i>.
+ * </p>
+ */
+class Challenge09Test {
+
+    Challenge09 challenge09;
+
+    @BeforeEach
+    void setup(){
+        challenge09 = new Challenge09();
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "12, 60",
+            "90, 14_760",
+            "208, 277_680",
+            "1000, 31_875_000",
+            "1450, 78_073_800",
+    })
+    void testPositiveCases(int sumOfTriplets,
+                           long expectedProductOfTriplet) {
+        assertThat(challenge09.calculate(triplet -> triplet.sum() == sumOfTriplets))
+                .isEqualTo(expectedProductOfTriplet);
+    }
+
+}
