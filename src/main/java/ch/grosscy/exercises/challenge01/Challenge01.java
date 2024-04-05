2008597c5cb70eee8644c238928a7237d306ed90
Nattaya Buck <cheekygirl765@gmail.com>
2024-04-05 13:26:20 +0200

    Implemented Challenge01

--- src/main/java/ch/grosscy/exercises/challenge01/Challenge01.java
+++ src/main/java/ch/grosscy/exercises/challenge01/Challenge01.java
@@ -5,7 +5,18 @@ public class Challenge01 implements ImplementationGuide {
     @Override
     public int calculate(int maximalNumber) {
         // TODO Implementation...
-        return 0;
+        int sum = 0;
+        for (int i = 0; i < maximalNumber; i++) {
+            if (i%3==0 && i%5==0) {
+                sum += i;
+            }
+        }
+        return sum;
     }
 
+    public static void main(String[] args) {
+        Challenge01 challenge01 = new Challenge01();
+        System.out.println();
+        System.out.println("The sum of all multiples is " + challenge01.calculate(1000));
+    }
 }
