dcc2cc7b8a7a8bff1cbbde167923a0edbe63999f
Cyril <92256856+groscy@users.noreply.github.com.>
2024-04-07 12:36:08 +0200

    Added Challenge12

---
+++ src/main/java/ch/grosscy/exercises/challenge12/Challenge12.java
@@ -0,0 +1,21 @@
+package ch.grosscy.exercises.challenge12;
+
+public class Challenge12 implements ImplementationGuide {
+
+    /**
+     * This challenge requires you to optimize your code if you get stuck follow
+     * these hints. (and don't lose your courage) ;)
+     * <br/>
+     * Implementation Hints:
+     * - Use the closed form for generating your triangle numbers
+     * - You don't have to search every number when you check for divisors
+     * - If you do not search every number you will have to calculate them another way
+     * - Leverage data structure to avoid duplicates
+     */
+    @Override
+    public long calculate(int numberOfDivisors) {
+        // TODO Implementation...
+        return 0L;
+    }
+
+}
---
+++ src/main/java/ch/grosscy/exercises/challenge12/ImplementationGuide.java
@@ -0,0 +1,7 @@
+package ch.grosscy.exercises.challenge12;
+
+public interface ImplementationGuide {
+
+    long calculate(int numberOfDivisors);
+
+}
