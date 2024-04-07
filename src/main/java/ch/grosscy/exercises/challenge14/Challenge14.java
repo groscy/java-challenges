46603bd4337434d9c1826fc5b9e5d54b4af80945
Cyril <92256856+groscy@users.noreply.github.com.>
2024-04-07 19:36:45 +0200

    Added Challenge14

---
+++ src/main/java/ch/grosscy/exercises/challenge14/Challenge14.java
@@ -0,0 +1,10 @@
+package ch.grosscy.exercises.challenge14;
+
+public class Challenge14 implements ImplementationGuide {
+
+    @Override
+    public long calculate(int upperLimit) {
+        // TODO Implementation...
+        return 0L;
+    }
+}
---
+++ src/main/java/ch/grosscy/exercises/challenge14/ImplementationGuide.java
@@ -0,0 +1,7 @@
+package ch.grosscy.exercises.challenge14;
+
+public interface ImplementationGuide {
+
+    long calculate(int upperLimit);
+
+}
